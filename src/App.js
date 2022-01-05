e680b658fcfb1d8cb3887300660df683fd209be6
hellin-eth <95247967+hellin-eth@users.noreply.github.com>
2022-01-05 02:41:15 +0100

    Usage of EVM event to get the id minted

--- src/App.js
+++ src/App.js
@@ -27,7 +27,10 @@ const App = () => {
     if (accounts.length !== 0) {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
-      setCurrentAccount(account)
+      setCurrentAccount(account);
+       // Setup listener! This is for the case where a user comes to our site
+          // and ALREADY had their wallet connected + authorized.
+          setupEventListener();
     } else {
       console.log("No authorized account found")
     }
@@ -55,13 +58,46 @@ const App = () => {
       */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]); 
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener() 
     } catch (error) {
       console.log(error)
     }
   }
 
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
   const askContractToMintNft = async () => {
-  const CONTRACT_ADDRESS = "0x343ee7e40fd3abe7d97b170d40337bf7cf4efd8b";
+  const CONTRACT_ADDRESS = "0xb73C0740815451a3b4BecaEB097d66Dd12581f37";
 
   try {
     const { ethereum } = window;
@@ -78,6 +114,9 @@ const App = () => {
       await nftTxn.wait();
       
       console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+      console.log(`Opensea link: https://testnets.opensea.io/assets/${nftTxn.hash}/INSERT_TOKEN_ID_HERE`);
+      console.log(`Rarible link: https://rinkeby.rarible.com/token/${nftTxn.hash}:INSERT_TOKEN_ID_HERE`);
+
 
     } else {
       console.log("Ethereum object doesn't exist!");
